914f06c82655b8647b69306af7798fa3eccd7ae0
Peter Lau <superpeterlau@outlook.com>
2022-06-25 20:01:28 +0800

    add query to api

--- netlify/functions/start/main.go
+++ netlify/functions/start/main.go
@@ -1,22 +1,59 @@
+// curl -siL https://courageous-cupcake-6d9019.netlify.app/.netlify/functions/start
+
 package main
 
 import (
   "github.com/aws/aws-lambda-go/events"
   "github.com/aws/aws-lambda-go/lambda"
+  "io/ioutil"
   "net/http"
+  "log"
+  "time"
+  "fmt"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+  url := request.QueryStringParameters["url"]
   return &events.APIGatewayProxyResponse{
     StatusCode:        200,
     Headers:           map[string]string{"Content-Type": "text/plain"},
     MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
-    Body:              "Hello, World!",
+    Body:              fmt.Sprintf("> URL: %s", url),
     IsBase64Encoded:   false,
   }, nil
 }
 
+func fetch(url string) string {
+  resp, err := http.Get(url)
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer resp.Body.Close()
+  if resp.StatusCode != 200 {
+    log.Fatalf("Failed to fetch data: %d %s", resp.StatusCode, resp.Status)
+  }
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    log.Fatal(err)
+  }
+  // []byte -> string
+  strBody := string(body)
+  log.Print(strBody)
+  return strBody
+  // doc, err := goquery.NewDocumentFromReader(resp.Body)
+  // if err != nil {
+  //   log.Fatal(err)
+  // }
+  // title := doc.Find("title").Text()
+  // log.Info(title)
+}
+
 func main() {
+  // url := "https://search.bilibili.com"
+  // fetch(url)
+  log.Print(time.Second)
+  log.Print(fmt.Sprintf("%s DONE", "Task one"))
+
   // Make the handler available for Remote Procedure Call
   lambda.Start(handler)
 }
